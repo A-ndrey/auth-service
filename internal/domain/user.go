1f09413a5341e85504ed9fb5977c301ac64a4cef
Andrey <anmokretsov@gmail.com>
2020-11-02 22:44:49 +0300

    use env

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -9,7 +9,7 @@ type ID int64
 
 type User struct {
 	ID        ID
-	Email     string
+	Email     string `gorm:"uniqueIndex"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt sql.NullTime
--- internal/driver/postgres.go
+++ internal/driver/postgres.go
@@ -2,12 +2,27 @@ package driver
 
 import (
 	"auth-service/internal/domain"
+	"errors"
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 func NewPostgresGorm() (*gorm.DB, error) {
-	dsn := "user=auth-user password=auth-passwd dbname=auth_service host=127.0.0.1"
+	user, ok := os.LookupEnv("POSTGRES_USER")
+	if !ok {
+		return nil, errors.New("env POSTGRES_USER not assigned")
+	}
+	password, ok := os.LookupEnv("POSTGRES_PASSWORD")
+	if !ok {
+		return nil, errors.New("env POSTGRES_PASSWORD not assigned")
+	}
+	dbname, ok := os.LookupEnv("POSTGRES_DB")
+	if !ok {
+		return nil, errors.New("env POSTGRES_DB not assigned")
+	}
+	dsn := fmt.Sprintf("user=%s password=%s dbname=%s host=127.0.0.1", user, password, dbname)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
