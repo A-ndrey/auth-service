b260c2090000f6302c4d2771f15fc5f77e92bdf4
Andrey <anmokretsov@gmail.com>
2020-11-17 23:09:13 +0300

    add graceful shutdown

--- cmd/auth-service/server.go
+++ cmd/auth-service/server.go
@@ -4,9 +4,14 @@ import (
 	"auth-service/internal/controller/handler"
 	"auth-service/internal/driver"
 	"auth-service/internal/service"
+	"context"
 	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -36,7 +41,26 @@ func main() {
 	handler.RouteHealth(&r.RouterGroup, healthService)
 	handler.RouteAPI(&r.RouterGroup, userService)
 
-	if err := r.Run(); err != nil {
-		log.Fatal(err)
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
 	}
 }
