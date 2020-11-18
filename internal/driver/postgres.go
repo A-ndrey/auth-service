72490d6c219789e2b7a1967a36839fe9c60853e1
Andrey <anmokretsov@gmail.com>
2020-11-19 00:33:39 +0300

    add Dockerfile

--- internal/driver/postgres.go
+++ internal/driver/postgres.go
@@ -14,16 +14,23 @@ func NewPostgresGorm() (*gorm.DB, error) {
 	if !ok {
 		return nil, errors.New("env POSTGRES_USER not assigned")
 	}
+
 	password, ok := os.LookupEnv("POSTGRES_PASSWORD")
 	if !ok {
 		return nil, errors.New("env POSTGRES_PASSWORD not assigned")
 	}
+
 	dbname, ok := os.LookupEnv("POSTGRES_DB")
 	if !ok {
 		return nil, errors.New("env POSTGRES_DB not assigned")
 	}
 
-	dsn := fmt.Sprintf("user=%s password=%s dbname=%s host=127.0.0.1", user, password, dbname)
+	dbHost, ok := os.LookupEnv("POSTGRES_HOST")
+	if !ok {
+		return nil, errors.New("env POSTGRES_HOST not assigned")
+	}
+
+	dsn := fmt.Sprintf("user=%s password=%s dbname=%s host=%s", user, password, dbname, dbHost)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
